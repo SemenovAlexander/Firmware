f88028983ecc456794d792bcf1e2753fd2bec3e8
Ryan Rittenhouse <ryan@redpost.co>
2017-01-03 14:59:56 -0500

    LCD speed/flow fix for H2

--- Repetier Firmware/Repetier/Configuration.h
+++ Repetier Firmware/Repetier/Configuration.h
@@ -8,11 +8,11 @@ Rostock MAX v3 = 5
 Hacker H2 = 6
 */
 // ### Define your Printer Model here! ###
-#define PRINTER 5
+#define PRINTER 6
 
 // ### Define your motherboard here! ###
 // 301 = RAMBo    302 = MINI RAMBo
-#define MOTHERBOARD 301
+#define MOTHERBOARD 302
 
 // ##### Older Orions w/ATX had Y inverted and NEW PSU on orions needs opposite ###
 // 1 = ATX on older machines  2 = Rail style PSU on newer machines ############################
@@ -753,7 +753,7 @@ Hacker H2 = 6
 #define POWER_INVERTING 0
 #define KILL_METHOD 1
 #define GCODE_BUFFER_SIZE 2
-#define ACK_WITH_LINENUMBER 1  // will set to 0 when done debugging
+#define ACK_WITH_LINENUMBER 0
 #define WAITING_IDENTIFIER "wait"
 #define ECHO_ON_EXECUTE 1
 #define EEPROM_MODE 1
